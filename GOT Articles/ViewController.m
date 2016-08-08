6966959b00fd204c44b1ae8f1b4857bb8e8271fe
Lukasz Matuszczak <Poznan>
2016-08-08 18:11:23 +0200

    download data from REST

--- GOT Articles/ViewController.m
+++ GOT Articles/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "ArticleTableViewCell.h"
+#import "Article.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+
+@property (strong, nonatomic) NSMutableArray* arrayOfArticles;
+@property (strong, nonatomic) NSMutableArray* arrayOfFavourites;
 
 @end
 
@@ -16,12 +22,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.arrayOfArticles = [[NSMutableArray alloc] init];
+    [self fetchDataFromRemote];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) fetchDataFromRemote{
+
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:@"http://gameofthrones.wikia.com/api/v1/Articles/Top?expand=1&category=Characters&limit=75"]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                if (data.length > 0 && error == nil)
+                {
+                    NSDictionary *greeting = [NSJSONSerialization JSONObjectWithData:data
+                                                                             options:0
+                                                                               error:NULL];
+                    NSMutableArray * a=  [[greeting objectForKey:@"items"]  mutableCopy];
+              
+                    for (NSString * i  in a) {
+                        
+                        Article* tmp = [[Article alloc] init:[i valueForKey:@"title"]  ForThumbnail:[i valueForKey:@"thumbnail"] ForAbstract:[i valueForKey:@"abstract"]];
+                        [self.arrayOfArticles addObject:tmp];
+                        
+                        
+                    }
+                   
+                }
+                [self.myTableView reloadData];
+            }] resume];
+
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.arrayOfArticles count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    ArticleTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Article"];
+
+    return cell;
+}
+
+
 
 @end
