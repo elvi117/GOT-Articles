4fe9098c5947f93a196850d47c9399e027c09397
Lukasz Matuszczak <Poznan>
2016-08-09 14:34:54 +0200

    height for cell in row and long pressure recognition and filtering by favourites and Segue to detail view

--- GOT Articles/ViewController.m
+++ GOT Articles/ViewController.m
@@ -9,12 +9,16 @@
 #import "ViewController.h"
 #import "ArticleTableViewCell.h"
 #import "Article.h"
-
+#import "DetailViewController.h"
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *myTableView;
 
+@property (weak, nonatomic) IBOutlet UIButton *filterDataButtonOutlet;
 @property (strong, nonatomic) NSMutableArray* arrayOfArticles;
 @property (strong, nonatomic) NSMutableArray* arrayOfFavourites;
+@property (strong, nonatomic) NSMutableArray* arrayWithIndexOfCellWithMoreHeight;
+@property ( nonatomic) Boolean showFavourites;
+@property (strong, nonatomic) UILongPressGestureRecognizer* cellPress;
 
 @end
 
@@ -22,11 +26,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.showFavourites = false;
     self.arrayOfArticles = [[NSMutableArray alloc] init];
     self.arrayOfFavourites = [[NSMutableArray alloc] init];
+    self.arrayWithIndexOfCellWithMoreHeight = [[NSMutableArray alloc] init];
+    
     [self fetchDataFromRemote];
+    UILongPressGestureRecognizer* longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(onLongPress:)];
+    [self.myTableView addGestureRecognizer:longPressRecognizer];
+   
 }
+
+- (void)onLongPress:(UILongPressGestureRecognizer *)sender{
+    if (sender.state == UIGestureRecognizerStateBegan)
+    {
+        CGPoint point = [sender locationInView:self.myTableView];
+        NSIndexPath* indexPath = [self.myTableView indexPathForRowAtPoint:point];
+        if (indexPath != nil) {
+            if (![self.arrayWithIndexOfCellWithMoreHeight containsObject: [NSNumber numberWithLong: indexPath.row]])
+                [self.arrayWithIndexOfCellWithMoreHeight addObject: [NSNumber numberWithLong: indexPath.row]];
+            else
+                [self.arrayWithIndexOfCellWithMoreHeight removeObject: [NSNumber numberWithLong: indexPath.row]];
+            [self.myTableView beginUpdates];
+            [self.myTableView endUpdates];
+        }
+    }
+}
+
 
 -(void) fetchDataFromRemote{
 
@@ -58,7 +84,11 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.arrayOfArticles count];
+    if (self.showFavourites)
+          return [self.arrayOfFavourites count];
+    else
+          return [self.arrayOfArticles count];
+  
 }
 
 
@@ -66,17 +96,58 @@
 {
     
     ArticleTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Article"];
-    [cell setArticleObject:[self.arrayOfArticles objectAtIndex:indexPath.row ] index: indexPath.row];
+    Boolean arrayExistFavouriteObject = [self.arrayOfFavourites containsObject:[self.arrayOfArticles objectAtIndex:indexPath.row ]];
+    if (!self.showFavourites)
+         [cell setArticleObject:[self.arrayOfArticles objectAtIndex:indexPath.row ] index: indexPath.row isFavourite: arrayExistFavouriteObject];
+     else
+         [cell setArticleObject:[self.arrayOfFavourites objectAtIndex:indexPath.row ] index: indexPath.row isFavourite: true] ;
+    
     cell.delegateMethod = self;
     return cell;
 }
 
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    DetailViewController *vcToPushTo = segue.destinationViewController;
+    vcToPushTo.delegateMethod = self;
+    vcToPushTo.isFavourite = true;
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+   
+    int rowHeight;
+    if ([self.arrayWithIndexOfCellWithMoreHeight containsObject: [NSNumber numberWithLong: indexPath.row]]) {
+        ArticleTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Article"];
+       
+        
+        rowHeight = cell.frame.size.height;
+        ;
+    } else rowHeight = 125.0f;
+    return rowHeight;
+}
+
+
 -(void) forwardIndex: (NSInteger) index isFavourite: (Boolean) is{
+    if(!self.showFavourites){
     if (is)
         [self.arrayOfFavourites addObject:[self.arrayOfArticles objectAtIndex:index]];
     else
-        [self.arrayOfFavourites removeObject:[self.arrayOfArticles objectAtIndex:index]];
+        [self.arrayOfFavourites removeObject:[self.arrayOfArticles objectAtIndex:index]];}
+    else
+        [self.arrayOfFavourites removeObjectAtIndex:index];
+    
+    [self.myTableView reloadData];
     NSLog(@"%@", self.arrayOfFavourites);
+}
+
+- (IBAction)filterDataButtonClick:(id)sender {
+    self.showFavourites = !self.showFavourites;
+    [self.arrayWithIndexOfCellWithMoreHeight removeAllObjects];
+    [self.myTableView reloadData];
+    
 }
 
 @end
